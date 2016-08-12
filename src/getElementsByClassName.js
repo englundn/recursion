b7d1665c4609d69fbb9cf8857a294d2c2e61a44f
Nick Englund <englundn@gmail.com>
2016-08-12 01:38:23 -0700

    Got getElementsByClassName working

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,10 +1,29 @@
-// If life was easy, we could just do things the easy way:
-// var getElementsByClassName = function (className) {
-//   return document.getElementsByClassName(className);
-// };
+var getElementsByClassName = function(className, startNode) {
+  node = startNode || document.body;
+  var found_elements = [];
 
-// But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+	var isInList = function(item, list) {
+    temp_list = Array.from(list);
+    inList = false;
+  	while(temp_list.length > 0) {
+    	if (item == temp_list.pop()) {
+        inList = true;
+        break;
+      }
+    }
+    return inList;
+  }
+  if (node.classList) {
+    if (isInList(className, node.classList)) {
+	    found_elements.push(node);
+    };
+	};
+	var childNodes = node.childNodes;
+	if(childNodes) {
+		for (var i=0; i<childNodes.length; i++){
+			var child = childNodes[i];
+			found_elements = found_elements.concat(getElementsByClassName(className, child));
+		};
+	};
+	return found_elements;
 };
